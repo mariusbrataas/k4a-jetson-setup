0b02da56b808a3c2227872d0b73f37dd7ea97fe6
Marius Brataas <mariusbrataas@me.com>
2021-01-26 11:13:31 +0100

    Update KINECT.sh

--- KINECT.sh
+++ KINECT.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt update && upgrade
-sudo apt install curl
+sudo apt install -y curl
 
 # Add microsoft repo
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
@@ -8,8 +8,8 @@ sudo apt-add-repository https://packages.microsoft.com/ubuntu/18.04/multiarch/pr
 sudo apt-get update
 
 # Setup kinect
-sudo apt install k4a-tools
-sudo apt install libk4a1.4-dev
+sudo apt install -y k4a-tools
+sudo apt install -y libk4a1.4-dev
 
 # Add rules
 sudo wget https://raw.githubusercontent.com/microsoft/Azure-Kinect-Sensor-SDK/develop/scripts/99-k4a.rules -O /etc/udev/rules.d/99-k4a.rules
